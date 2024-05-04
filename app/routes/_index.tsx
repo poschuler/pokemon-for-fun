91e8d87f9df4041f4284365bc88819d4a72b14ec
poschuler <poschuler@gmail.com>
2024-05-03 21:24:51 -0500

    vercel_again

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
-import { redirect } from "@remix-run/node";
+export async function loader({ request, response }) {
+  response.status = 302;
+  response.headers.set("Location", "/search");
+  throw response;
+}
 
-export const loader = () => {
-  return redirect("/home/main");
-};
+export default function Index() {}
